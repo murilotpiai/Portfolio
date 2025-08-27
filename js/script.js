7cf4f4ba7f96151e86890d2b12927e44661e2a2c
Murilo Turcato Piai <murilotpiai@gmail.com>
2025-08-27 09:22:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+const toggleBtn = document.getElementById("toggle-btn");
 
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "☀️"; 
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "🌙"; 
+  }
+}
+
+
+function loadTheme() {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+      toggleBtn.textContent = "☀️";
+    }
+  } else {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.body.classList.add("dark-mode");
+      toggleBtn.textContent = "☀️";
+    }
+  }
+}
+
+toggleBtn.addEventListener("click", toggleDarkMode);
+loadTheme();
